caab3e4c0db619086df8d8a9bc2200d62132fa64
MadsDueKristensen <32197042+MadsDueKristensen@users.noreply.github.com>
2019-03-04 12:10:17 +0100

    Comments

--- part_01/PQHeap.java
+++ part_01/PQHeap.java
@@ -2,12 +2,20 @@
  * DM507, Software Engineering
  * 
  * @author Mads Due Kristensen, madkr17@student.sdu.dk
+ * @auther Victor Gram Thomsen, vitho17@student.sdu.dk
+ * @auther Jeppe Hannibal Niemann, niema17@student.sdu.dk 
  */
 public class PQHeap implements PQ {
 
+    // Array with Element's
     private Element[] elements;
+    // The current heap size
     private int heapSize;
 
+   /**
+    * This is the main constructor.
+    * @param maxElms the max size of the array.
+   */
     public PQHeap(int maxElms) {
         elements = new Element[maxElms - 1];
     }
@@ -34,6 +42,12 @@ public class PQHeap implements PQ {
         heapSize++;
     }
 
+   /**
+    * This method returns a array of Elements in heap order with the smallest Element in the top.
+    * @param A array with elements.
+    * @param i index for the current element.
+    * @return array of Elements that has been minheapified.
+   */       
     private Element[] MinHeapify(Element[] A, int i) {
         int l = Left(i);
         int r = Right(i);
@@ -56,14 +70,29 @@ public class PQHeap implements PQ {
         return A;
     }
 
+   /**
+    * This method returns the index of the parent for the param.
+    * @param i element index.
+    * @return the parent index
+   */    
     private int Parrent(int i) {
         return i / 2;
     }
 
+   /**
+    * This method returns the index of the element to the left.
+    * @param i element index.
+    * @return index of element to the left
+   */      
     private int Left(int i) {
         return 2 * i;
     }
 
+   /**
+    * This method returns the index of the element to the right.
+    * @param i element index.
+    * @return index of element to the right
+   */       
     private int Right(int i) {
         return 2 * i + 1;
     }
