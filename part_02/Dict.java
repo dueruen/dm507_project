0df3f5bf33b8271727004467acd173569da3009b
dueruen <32197042+dueruen@users.noreply.github.com>
2019-03-21 15:48:44 +0100

    Part2 setup

---
+++ part_02/Dict.java
@@ -0,0 +1,5 @@
+public interface Dict {
+    public void insert(int k);
+    public int[] orderedTraversal();
+    public boolean search(int k);
+}
---
+++ part_02/DictBinTree.java
@@ -0,0 +1,18 @@
+public class DictBinTree implements Dict {
+
+    @Override
+    public void insert(int k) {
+
+    }
+
+    @Override
+    public int[] orderedTraversal() {
+        return null;
+    }
+
+    @Override
+    public boolean search(int k) {
+        return false;
+    }
+
+}
