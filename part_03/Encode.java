02e814dc351d46f2d3ce2047c467f44a0357df37
dueruen <32197042+dueruen@users.noreply.github.com>
2019-04-26 14:19:19 +0200

    Scan and write

--- part_03/Encode.java
+++ part_03/Encode.java
@@ -1,3 +1,44 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class Encode {
-    
+    public Encode(String[] args) {
+
+    }
+
+    private int[] scanInputFile(String inputFile) {
+        int[] frequency = new int[256]; 
+        try (FileInputStream inFile = new FileInputStream(inputFile)) {
+            int b;
+			while ((b = inFile.read()) != -1) {
+				frequency[b] += 1;
+			}
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+        return frequency;
+    }
+
+    private void writeOutputFile(String[] args, String[] keyTable, int[] frequency) {
+        try (
+            FileInputStream inFile = new FileInputStream(args[0]); 
+            BitOutputStream out = new BitOutputStream(new FileOutputStream(args[1]))
+        ) {
+            for (int i : frequency) {
+                out.writeInt(i);
+            }
+            int b;
+			while ((b = inFile.read()) != -1) {
+                for (String s : keyTable[b].split()) {
+                    out.writeBit(Integer.parseInt(s));
+                }
+			}
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) { 
+        Encode e = new Encode(args);
+    }
 }
