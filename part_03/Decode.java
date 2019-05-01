391677c17666cc1f1c6f26efbc11b3eea2f2c20d
GGkarma <32382976+GGkarma@users.noreply.github.com>
2019-05-01 11:35:36 +0200

    In progress

--- part_03/Decode.java
+++ part_03/Decode.java
@@ -1,3 +1,18 @@
+import java.io.InputStream;
+import java.io.FileInputStream;
+
 public class Decode {
-    
+
+    public Decode(String file) {
+        try {
+            BitInputStream inFile = new BitInputStream(file);
+            inFile.BitInputStream(file);
+            inFile.readBit();
+            inFile.readint(file);
+            inFile.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 }
