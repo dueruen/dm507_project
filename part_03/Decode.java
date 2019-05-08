81e536cc6e2ba8006049144ee0ba7d9fbcd11aed
GGkarma <32382976+GGkarma@users.noreply.github.com>
2019-05-08 10:50:13 +0200

    fix

--- part_03/Decode.java
+++ part_03/Decode.java
@@ -7,13 +7,39 @@ public class Decode {
         try {
             FileInputStream fin = new FileInputStream(inputFile);
             BitInputStream inFile = new BitInputStream(fin);
-            inFile.readBit();
-            inFile.readint(fin);
-            Encode.houfman();
+            for()
             inFile.close();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
+    public Element huffman(int[] input) {
+        int n = Math.abs(input.length);
+        PQ q = populateQueue(input);
+        for (int i = 0; i < n; i++) {
+            Element left = q.extractMin();
+            Element x = left;
+            Element right = q.extractMin();
+            Element y = right;
+            int key = x.getKey() + y.getKey();
+
+            DictBinTree tree = new DictBinTree();
+            tree.insert(key);
+            tree.insert(x.getKey());
+            tree.insert(y.getKey());
+
+            q.insert(new Element(key, tree));
+
+        }
+        return q.extractMin();
+    }
+
+    private PQ populateQueue(int[] input) {
+        PQ q = new PQHeap(input.length);
+        for (int i : input) {
+            q.insert(new Element(i, null));
+        }
+        return q;
+    }
 }
